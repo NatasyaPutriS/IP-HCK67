f50f74d8a9fa7300a9569a31a888e68bcfb3a087
NatasyaPutriS <natasyaputri.s@gmail.com>
2024-02-14 18:19:35 +0700

    IProject Phase 2 Feat: User Login with Credentials

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -41,4 +41,35 @@ const addUser = async (req, res) => {
     }
 }
 
-module.exports = { addUser }
+const loginWithCredentials = async (req, res) => {
+    try {
+        const body = req.body
+        const {email, password} = body
+
+        if (!email || !password) {
+            return res.status(400).json({message: "All fields are required"})
+        }
+
+        const userCredential = await signInWithEmailAndPassword(
+            auth, email, password
+        )
+
+        const user = userCredential.user
+
+        // Save refreshToken to cookie
+        let refreshToken = user?.stsTokenManager.refreshToken
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: false,
+            maxAge: 24 * 60 * 60 * 1000
+        })
+
+        res.status(200).json({ message: "User logged in successfully", user})
+    } catch (error) {
+        res
+        .status(500)
+        .json({message: "Error adding data", error: error.message})
+    }
+}
+
+module.exports = { addUser, loginWithCredentials }
